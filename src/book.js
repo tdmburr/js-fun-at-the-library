7cf1aa877d231c50ea8a136528a1c0ca769aeea4
Trey Marshall <marshall.trey92@yahoo.com>
2022-12-01 18:33:49 -0600

    Add work on book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,42 @@
-function createTitle() {
+function createTitle(title) {
+return `The ${title}` 
   
 }
 
+function buildMainCharacter(n, a, p) {
+return {
+  name: n,
+  age: a,
+  pronouns: p
+} 
+
+}
+
+function saveReview(comment, array) {
+if (array.includes(comment)) {  
+return array
+} else { 
+  return array.push(comment);
+}
+  
+}  
+
+function calculatePageCount() {
+
+}
+
+function writeBook() {
+
+}
+
+function editBook(){
+
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
