b0299758709bb48abde1e4d4983cad97be6d0c6e
Trey Marshall <marshall.trey92@yahoo.com>
2022-12-03 15:50:42 -0600

    Complete shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,40 @@
+function shelfBook(book, shelf) {
+if (shelf.length < 3)  
+  return shelf.unshift(book)
+}
+
+
+function unshelfBook(book, shelf) {
+for (var i = 0; i < shelf.length; i++) {
+  if (book === shelf[i].title) {
+    return shelf.splice(i, 1)
+  }
+}   
+}
+
+function listTitles(shelf) {
+var title1 = []
+for (var i = 0; i < shelf.length; i++) {
+title1.push(shelf[i].title)
+} return title1.join(", ")
+}
+
+function searchShelf(shelf, title) { 
+for (var i = 0; i < shelf.length; i++) {
+  if (shelf[i].title === title) {
+    return true
+  }
+  }
+    return false
+}
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
